5fd25a349cc3822e526f1aa0986b40dee4e0b2d5
Michael L. Collard <collard@uakron.edu>
2021-10-20 15:59:20 -0400

    Add sort for one-element vector

--- sortTest.cpp
+++ sortTest.cpp
@@ -20,5 +20,17 @@ int main() {
         assert(v.size() == 0);
     }
 
+    {
+        std::vector<int> v{ 1 };
+
+        assert(!v.empty());
+        assert(v.size() == 1);
+        assert(v == std::vector<int>({ 1 }));
+        sort(v);
+        assert(!v.empty());
+        assert(v.size() == 1);
+        assert(v == std::vector<int>({ 1 }));
+    }
+
     return 0;
 }
